1f31a0c93415818e5f1fc1dff3f34c0217b31799
liu.mengrong <liu.mengrong@westone.com.cn>
2018-11-02 16:49:29 +0800

    开始加入fragment

--- app/src/main/java/com/ohayo/loveclass/LcMain.java
+++ app/src/main/java/com/ohayo/loveclass/LcMain.java
@@ -2,12 +2,31 @@ package com.ohayo.loveclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LcMain extends AppCompatActivity {
+
+    @BindView(R.id.tab_menu)
+    RadioGroup RgMenus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lc_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        RgMenus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            }
+        });
     }
 }
